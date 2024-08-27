63834ee9c65a3ee7e4c360f80090efb2321a41aa
singhyashe15 <singhyashraj574@gmail.com>
2024-08-28 01:28:02 +0530

    add hex color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#121212',
+        lightGray: '#E0E0E0',
+      },
+    },
   },
   plugins: [],
 }
