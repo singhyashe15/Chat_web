ef44436e34a2318a7ff0a544431e57df4cfd6010
Yashraj Singh <130179859+singhyashe15@users.noreply.github.com>
2024-09-21 01:15:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ const {app,server} = require('./socket/socket')
 // connect fronted 
 app.use(cors({
   origin: 'https://chat-web-rho-nine.vercel.app/',
-  credentials:true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow necessary methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
+  optionsSuccessStatus: 200
 }));
 
 app.use(express.json());
