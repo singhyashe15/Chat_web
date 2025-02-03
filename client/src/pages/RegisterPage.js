b6636abe39e5f84f67dec0c6101f16357d248d81
Yashraj Singh <130179859+singhyashe15@users.noreply.github.com>
2025-02-03 10:27:42 +0530

    final commit done

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -87,17 +87,8 @@ const RegisterPage = () => {
   return (
     <div className='mt-20  h-auto flex flex-col lg:flex-row justify-evenly items-center ' >
         <div className='bg-white w-full max-w-md  rounded-[4.25rem] overflow-hidden p-4 m-8 border-2 border-blue-900'>
-          <h3 className='text-center font-bold italic text-lg'>Welcome to Chat Wave</h3>
-          <div className='h-4 -mt-4' >
-            <img
-              src={chatLogo} //logo
-              width={20}
-              height={20}
-              alt='logo'
-              className='w-80 mx-auto'
-              />
-          </div>
-          <form className='grid gap-4 mt-5' onSubmit={handleSubmit}>
+          <h3 className='text-center font-bold italic text-lg'>Welcome to Chatify </h3>
+          <form className='grid gap-4 mt-5' >
               <div className='flex flex-col gap-1 mt-5'>
                 <input
                   type='text'
@@ -166,13 +157,11 @@ const RegisterPage = () => {
               </div>
 
 
-              <button
-               className='bg-slate-500 text-lg  px-4 py-1  rounded-full mt-2 font-bold text-black leading-relaxed tracking-wide hover:bg-violet-500'>
+              <Button colorScheme='cyan' px-4 py-1 mt-2 onClick={handleSubmit} >
                 Register
-                 </button>
+              </Button>
 
           </form>
-
           <p className='my-3 text-center'>Already have account ? <Link to={"/login"} className='hover:text-green-700 font-semibold'>LOGIN</Link></p>
         </div>
     </div>
