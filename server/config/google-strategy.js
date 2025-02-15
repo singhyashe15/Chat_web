9ad1d9ed8b5f3969c8b931d1ee8621f0177613e1
Yashraj Singh <130179859+singhyashe15@users.noreply.github.com>
2025-02-15 23:57:21 +0530

    final

--- server/config/google-strategy.js
+++ server/config/google-strategy.js
@@ -11,7 +11,7 @@ dotenv.config()
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: `${process.env.BACKENED_URL}/auth/google/callback`
 },
 async function(accessToken, refreshToken, profile, cb) {
   try {
