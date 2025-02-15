a79eaec273f3054b8e2e9cec57592253b398f216
Yashraj Singh <130179859+singhyashe15@users.noreply.github.com>
2025-02-15 23:37:56 +0530

    testing

--- server/index.js
+++ server/index.js
@@ -29,10 +29,14 @@ app.use(passport.initialize());
 //app.get('/auth/google', passport.authenticate('google', {session: false,scope: ['profile','email'] }));
 
 app.get('/auth/google', (req, res) => {
-  const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.Fronted_Url}/auth/callback&response_type=code&scope=email%20profile`;
+  const authUrl = `https://accounts.google.com/o/oauth2/auth
+    ?client_id=${process.env.GOOGLE_CLIENT_ID}
+    &redirect_uri=https://chat-web24.onrender.com/auth/google/callback
+    &response_type=code
+    &scope=email%20profile`;
 
-  console.log("OAuth URL: ", authUrl);  // Check this in server logs
-  res.redirect(authUrl);
+    console.log("OAuth URL: ", authUrl);  // Log this URL in your backend
+    res.redirect(authUrl);
 });
 app.get(
   "/auth/google/callback",
